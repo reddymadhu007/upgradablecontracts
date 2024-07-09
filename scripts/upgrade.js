7ff42c217ed3c12741ec3d62403b165f7a15d21c
reddymadhu007 <nr00795359@techmahindra.com>
2024-07-09 21:58:58 +0530

    edited example text

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -6,7 +6,7 @@ async function main() {
   );
   console.log("Upgrading ERC20UpgradableV1...");
   await upgrades.upgradeProxy(
-    "0xC81cBaB47B1e6D6d20d4742721e29f22C5835dcB",
+    "0xA4dD31FA5baa2e8D747A1b67dF13ACc41f0F2c22",
     ERC20UpgradableV2
   );
   console.log("Upgraded Successfully");
